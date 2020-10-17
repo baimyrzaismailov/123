c3dd0dcb07ba4de3bf92053f54892c4525d03253
baimyrza <baimyrza.ismailov@gmail.com>
2020-10-17 17:58:50 +0600

    first upload

---
+++ src/libs/jquery/script.js
@@ -0,0 +1,10 @@
+$('.burger').click(function () {
+    $('.burger span').toggleClass('active')
+    $('.header .nav').toggleClass('active')
+})
+
+
+// let burger = document.getElementById('burger')
+//     burger.addEventListener('click', function () {
+//
+// })
